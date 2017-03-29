953b70801bbd664feaae4f34bdc3caf7dde6bd0b
Jaap Jansma <jaap@edeveloper.nl>
2017-03-29 11:41:00 +0200

    added currency conversion and fixed minor issues

---
+++ pum_claims.js
@@ -0,0 +1,30 @@
+(function ($) {
+    function pum_claims_convert_currency() {
+        var currency_id = $('#edit-currency').val();
+        var amount = parseFloat($('#edit-amount').val());
+        $('#euro_amount').html('0,00');
+        if (currency_id > 0 && amount != NaN) {
+            $.ajax({
+                url: Drupal.settings.pum_claims.currencyConvertUrl,
+                method: "GET",
+                data: {
+                    currency_id: currency_id,
+                    amount: amount
+                },
+                dataType: "html",
+                success: function (data) {
+                    $('#euro-amount').html(data);
+                }
+            });
+        }
+
+    }
+
+    Drupal.behaviors.ajaxExample = {
+        attach: function (context, settings) {
+            $('#edit-currency', context).change(pum_claims_convert_currency);
+            $('#edit-amount', context).change(pum_claims_convert_currency);
+        }
+    };
+
+}(jQuery));
