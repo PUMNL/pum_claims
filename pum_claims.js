5cc3364202e607bb2bb345da8e101b92e5a789ff
Jaap Jansma <jaap@edeveloper.nl>
2017-05-30 15:44:49 +0200

    Fixed #65 geen expense datum in de toekomst

--- pum_claims.js
+++ pum_claims.js
@@ -2,14 +2,17 @@
     function pum_claims_convert_currency() {
         var currency_id = $('#edit-currency').val();
         var amount = parseFloat($('#edit-amount').val());
+        var expense_date = $('#edit-expense-date-datepicker-popup-0').val();
         $('#euro_amount').html('0,00');
-        if (currency_id > 0 && amount != NaN) {
+        if (currency_id > 0 && amount != NaN && expense_date.length > 0) {
+
             $.ajax({
                 url: Drupal.settings.pum_claims.currencyConvertUrl,
                 method: "GET",
                 data: {
                     currency_id: currency_id,
-                    amount: amount
+                    amount: amount,
+                    conversion_date: expense_date
                 },
                 dataType: "html",
                 success: function (data) {
@@ -22,6 +25,7 @@
 
     Drupal.behaviors.claims = {
         attach: function (context, settings) {
+            $('#edit-expense-date-datepicker-popup-0', context).change(pum_claims_convert_currency);
             $('#edit-currency', context).change(pum_claims_convert_currency);
             $('#edit-amount', context).change(pum_claims_convert_currency);
             $('.line-amount-eur').hover(
